a9b899aa5abbbb1b12efbeb138b7614957e47632
Edgar <edgarsolisromeu@gmail.com>
2022-02-13 02:42:58 +0700

    Added function for creating grids with function for create button, but the program is too slow

--- scripts.js
+++ scripts.js
@@ -4,32 +4,42 @@ let gridSize = document.querySelector('.grid-size');
 const grid = document.querySelector('.grid');
 const create = document.querySelector('.create');
 const erase = document.querySelector('.erase'); 
-//let selectedColour = document.querySelector(''); 
+// let selectedColour = document.querySelector(''); 
 let selectedSize = 16;
 
-/*
 createGrid(selectedSize);
+
 
 function createDiv(size){
     const content = document.createElement('div');
     content.classList.add('box');
-    content.style.cssText = 'border: 4px solid black; padding: 4px';
     content.style.width = `${size}px`;
+    content.style.height = `${size}px`;
+    return content;
 }
 
 
-
-function createGrid(gridValue){
-    for(let i = 0;i< gridValue;i++){
-        for(let j = 0;j< gridValue;j++){
-            grid.appendChild(createDiv(grid.clientWidth));
+function createGrid(selectedSize){
+    for(let i = 0;i< selectedSize;i++){
+        for(let j = 0;j< selectedSize;j++){
+            grid.appendChild(createDiv(grid.clientWidth/ selectedSize));
         }
     }
+}
 
-}*/
+function eraseGrid(){
+    grid.innerHTML = '';
+    createGrid(selectedSize);
+}
 
 gridValue.addEventListener('input', function(e){
     selectedSize = e.target.value;
     console.log(selectedSize);
     gridSize.textContent = `${selectedSize} X ${selectedSize}`;
 });
+
+create.addEventListener('click', function(){
+    eraseGrid();
+});
+
+
