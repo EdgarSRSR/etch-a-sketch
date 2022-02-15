5798041e8b177377e84fdd8e377f20ff2046c0ff
Edgar <edgarsolisromeu@gmail.com>
2022-02-16 01:28:58 +0700

    added comments

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Variables
 const container = document.querySelector('#container');
 let gridValue = document.querySelector('input');
 let gridSize = document.querySelector('.grid-size');
@@ -11,25 +12,21 @@ const chooseColor = document.querySelector('#colorpalette');
 let selectedSize = 16;
 let selectedColour = 'black';
 
-// grid.addEventListener('mousedown', function(e){
-//     if(e.target.matches('.box')){
-//         e.target.classList.add('activated')
-//     }
-// });
-
+// Removes all childs of a node, currently unused
 function removeAllChildNodes(parent){
     while(parent.firstChild){
         parent.removeChild(parent.firstChild);
     }
 }
 
-
+// Sets a new value for the selectedSize variable and displays it on the control panel
 gridValue.addEventListener('input', function(e){
          selectedSize = e.target.value;
          console.log(selectedSize);
          gridSize.textContent = `${selectedSize} X ${selectedSize}`;
     });
 
+// Creates the grid and the first stroke of the painting
 function createGrid (selectedSize) {
     grid.setAttribute('style', `grid-template-columns: repeat( ${selectedSize}, 2fr); grid-template-rows: repeat( ${selectedSize}, 2fr);`);
     for(let i = 0; i < selectedSize * selectedSize;i++){
@@ -46,24 +43,29 @@ function createGrid (selectedSize) {
     }
 };
 
+// Erases the html inside of the gris, it is the alternative to removeAllChildNodes in use
 function eraseGrid(){
         grid.innerHTML = '';
         createGrid(selectedSize);
 }
 
+// Create button
 create.addEventListener('click', function(){
     eraseGrid();
 });
 
+// Erase button
 erase.addEventListener('click', function(){
     eraseGrid();
 });
 
+// Black button
 black.addEventListener('click', function(){
     selectedColour = 'black';
     console.log(selectedColour);
 });
 
+// Helps painting the grid
 function paintGrid(e, color){
     if(e.buttons == 1){
         if(e.target.matches('.box')){
@@ -74,6 +76,7 @@ function paintGrid(e, color){
     }
 }
 
+// Creates a random coour for multicolor button
 function getRandomColour(){
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
@@ -83,11 +86,13 @@ function getRandomColour(){
 
 }
 
+// Multicolour button
 multicolour.addEventListener('click', function(){
     selectedColour = 'random';
     console.log(selectedColour);
 });
 
+// Choose colour input
 chooseColor.addEventListener("change", watchColorPicker, false);
 function watchColorPicker(event) {
   selectedColour = event.target.value;
@@ -109,6 +114,7 @@ function watchColorPicker(event) {
 // });
 
 
+// When you click on the grid it activates the paintGrid function
 grid.addEventListener('mousedown', event => {
     if(event.buttons == 1){
         let cell = grid.children;
@@ -124,6 +130,7 @@ grid.addEventListener('mousedown', event => {
     }
 });
 
+// When grid button is pressed it creates a square grid to know where the boxes are, it can be turned on and off
  gridmarks.addEventListener('click', function(){
      let cell = grid.children;
      for(let i = 0; i < selectedSize * selectedSize; i++){
@@ -131,6 +138,5 @@ grid.addEventListener('mousedown', event => {
         }
  });
 
-
-
+// Initializes the grid
 createGrid(selectedSize);
